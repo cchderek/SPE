af90aaed5c82a3a967eb8897b9b9d4ca37d6da0c
cchderek <cchderek@gmail.com>
2016-12-02 00:53:43 +0100

    Update

--- draft2.m
+++
@@ -1,2 +0,0 @@
-
-answer = strsplit(char(inputdlg('Please recall as many as words possible, separated by a space')));
--- untitled2.m
+++
@@ -1,3 +0,0 @@
-User_Answer = {'apple', 'obama', 'donald'}
-
-stimuli = {'apple','orange','rat','cat','donald','obama'}
